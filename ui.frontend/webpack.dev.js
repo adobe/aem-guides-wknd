35b8583748fde0797dfb670a750428d97f7b3522
Daniel Gordon <dgordon@adobe.com>
2019-09-15 16:00:13 -0700

    added static webpack server

--- ui.frontend/webpack.dev.js
+++ ui.frontend/webpack.dev.js
@@ -1,8 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
    mode: 'development',
    devtool: 'inline-source-map',
-   performance: {hints: "warning"}
+   performance: { hints: "warning" },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.resolve(__dirname, './src/static/article.html')
+      })
+   ],
+   devServer: {
+      inline: true,
+      proxy: [{
+         context: ['/content', '/etc.clientlibs'],
+         target: 'http://localhost:4502',
+      }]
+   }
 });
