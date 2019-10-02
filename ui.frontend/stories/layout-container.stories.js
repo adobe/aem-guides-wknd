460b80d1a5f6f3202b68455a070ae70f54e4fb42
david g <davidjgonzalez@gmail.com>
2019-10-02 16:46:33 -0400

    KT-3331 - Made Sign In/Sign Out form re-usable across Sign In and Sign Out use-cases.

--- ui.frontend/stories/layout-container.stories.js
+++ ui.frontend/stories/layout-container.stories.js
@@ -14,61 +14,70 @@ class SignInLayoutContainer {
     get markup() {
         return `<div class="xf-web-container">                    
                     <div class="container">
-                        <div class="root responsivegrid ${this.styleClass}">
-                   
+                        <div class="root responsivegrid">
                             <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
-                    
-                                <div class="title cmp-title--underline aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-title">
-                                        <h1 class="cmp-title__text">Sign in</h1>
-                                    </div>
-                                </div>
-                                <div class="title aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-title">
-                                        <h3 class="cmp-title__text">Welcome Back</h3>
-                                    </div>
-                                </div>
-                                <div class="log-in aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="wknd-sign-in-form">
-                                        <form method="POST" action="/libs/granite/core/content/login.html/j_security_check"
-                                              class="wknd-sign-in-form__form">
-                                            <input type="hidden" name="charset" value="🐶"/>
-                                            <input type="hidden" name="resource" value="./"/>
-                                            <input type="hidden" name="j_validate" value="true"/>
-                    
-                                            <div class="wknd-sign-in-form__field wknd-sign-in-form__field--text-input">
-                                                <div class="cmp-form-text" data-cmp-is="formText">
-                                                    <input class="cmp-form-text__text" data-cmp-hook-form-text="input" aria-label="EMAIL"
-                                                           type="text" id="form-text-1976236455" placeholder="EMAIL" name="j_username"
-                                                           required/>
-                                                </div>
+                                <div class="responsivegrid cmp-layout-container--modal aem-GridColumn aem-GridColumn--default--12">
+                                    <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                        <div class="title cmp-title--underline aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-title">
+                                                <h1 class="cmp-title__text">Sign In</h1>
                                             </div>
-                                            <div class="wknd-sign-in-form__field wknd-sign-in-form__field--text-input">
-                                                <div class="cmp-form-text" data-cmp-is="formText">
-                                                    <input class="cmp-form-text__text" data-cmp-hook-form-text="input" aria-label="PASSWORD"
-                                                           type="text" id="form-text-1499668257" placeholder="PASSWORD" name="j_password"
-                                                           required/>
-                                                </div>
+                                        </div>
+                                        <div class="title aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-title">
+                                                <h3 class="cmp-title__text">Welcome Back</h3>
                                             </div>
-                                            <div class="wknd-sign-in-form__field wknd-sign-in-form__field--text">
-                                                <div class="cmp-text">
-                                                    <a href="#">FORGOT YOUR PASSWORD?</a>
-                                                </div>
+                                        </div>
+                                        <div class="sign-in-form aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="wknd-sign-in-form">
+                                                <form method="POST" action="/system/sling/login/j_security_check" id="wknd-sign-in-form"
+                                                      class="wknd-sign-in-form__form">
+                                                    <input type="hidden" name="charset" value="🐶"/>
+                                                    <input type="hidden" name="j_validate" value="true"/>
+                                                    <input type="hidden" name="sling.auth.redirect"
+                                                           value="/content/wknd/language-masters/en.html"/>
+                                                    <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                        <div class="text aem-GridColumn aem-GridColumn--default--12">
+                                                            <div class="cmp-form-text" data-cmp-is="formText">
+                
+                
+                                                                <input class="cmp-form-text__text" data-cmp-hook-form-text="input"
+                                                                       aria-label="EMAIL" type="text" id="form-text-1660183001"
+                                                                       placeholder="EMAIL" name="j_username"/>
+                                                            </div>
+                                                        </div>
+                                                        <div class="text aem-GridColumn aem-GridColumn--default--12">
+                                                            <div class="cmp-form-text" data-cmp-is="formText">
+                
+                
+                                                                <input class="cmp-form-text__text" data-cmp-hook-form-text="input"
+                                                                       aria-label="PASSWORD" type="password" id="form-text-1010823116"
+                                                                       placeholder="PASSWORD" name="j_password"/>
+                                                            </div>
+                                                        </div>
+                                                        <div class="text aem-GridColumn aem-GridColumn--default--12">
+                                                            <div class="cmp-text">
+                                                                <p><a href="#">FORGOT YOUR PASSWORD?</a></p>
+                
+                                                            </div>
+                                                        </div>
+                                                        <div class="button aem-GridColumn aem-GridColumn--default--12">
+                                                            <button type="SUBMIT" class="cmp-form-button">SIGN IN</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
                                             </div>
-                                            <div class="wknd-sign-in-form__field wknd-sign-in-form__field--button">
-                                                <button type="SUBMIT" class="cmp-form-button">SIGN IN</button>
+                                        </div>
+                                        <div class="separator aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-separator">
+                                                <hr class="cmp-separator__horizontal-rule"/>
                                             </div>
-                                        </form>
-                                    </div>
-                                </div>
-                                <div class="separator aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-separator">
-                                        <hr class="cmp-separator__horizontal-rule"/>
-                                    </div>
-                                </div>
-                                <div class="text aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-text">
-                                        <p>NEED AN ACCOUNT? <a href="#">SIGN UP</a></p>
+                                        </div>
+                                        <div class="text aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-text">
+                                                <p>NEED AN ACCOUNT? <a href="#sign-up">SIGN UP</a></p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -87,35 +96,45 @@ class SignOutLayoutContainer {
 
     get markup() {
         return `<div class="xf-web-container">                    
-                    <div class="container">
-                        <div class="root responsivegrid ${this.styleClass}">
-
+                   <div class="container">
+                        <div class="root responsivegrid">
                             <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
-                                
-                                <div class="title cmp-title--underline aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-title">
-                                        <h1 class="cmp-title__text">Sign Out</h1>
+                                <div class="responsivegrid cmp-layout-container--modal aem-GridColumn aem-GridColumn--default--12">
+                                    <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                        <div class="title cmp-title--underline aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-title">
+                                                <h1 class="cmp-title__text">Sign Out</h1>
+                                            </div>
+                                        </div>
+                                        <div class="title aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-title">
+                                                <h3 class="cmp-title__text">Goodbye!</h3>
+                                            </div>
+                                        </div>
+                                        <div class="text aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-text">
+                                                <p>Thanks for stopping by, come back soon!</p>
+                                            </div>
+                                        </div>
+                                        <div class="sign-in-form aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="wknd-sign-in-form">
+                                                <form method="POST" action="/system/sling/login/j_security_check" id="wknd-sign-in-form" class="wknd-sign-in-form__form">
+                                                    <input type="hidden" name="charset" value="🐶"/>
+                                                    <input type="hidden" name="j_validate" value="true"/>
+                                                    <input type="hidden" name="sling.auth.redirect"/>
+                                                    <div class="aem-Grid aem-Grid--12 aem-Grid--default--12 ">
+                                                        <div class="button aem-GridColumn aem-GridColumn--default--12">
+                                                            <button type="SUBMIT" class="cmp-form-button" name="resource" value="/content/wknd/us/en.html">SIGN OUT</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="separator aem-GridColumn aem-GridColumn--default--12">
+                                            <div class="cmp-separator">
+                                                <hr class="cmp-separator__horizontal-rule"/>
+                                            </div></div>
                                     </div>
-                                </div>
-                                <div class="title aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-title">
-                                        <h3 class="cmp-title__text">Goodbye!</h3>
-                                    </div>
-                                </div>
-                                <div class="text aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-text">
-                                        <p>Thanks for stopping by, come back soon!</p>                                    
-                                    </div>
-                                </div>
-                                <div class="separator aem-GridColumn aem-GridColumn--default--12">
-                                    <div class="cmp-separator">
-                                        <hr class="cmp-separator__horizontal-rule"/>
-                                    </div>
-                                </div>
-                                <div class="button aem-GridColumn aem-GridColumn--default--12">
-                                    <a class="cmp-button" href="/system/sling/logout" aria-label="Sign Out">
-                                        <span class="cmp-button__text">SIGN OUT</span>
-                                    </a>
                                 </div>
                             </div>
                         </div>
