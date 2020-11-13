2816c4acf2c404cba0116cc94255269ab0c8724b
Daniel Gordon <dgordon@adobe.com>
2020-11-13 15:17:44 -0800

    remove ajax login with standard redirect

--- ui.frontend/src/main/webpack/site/main.js
+++ ui.frontend/src/main/webpack/site/main.js
@@ -8,6 +8,5 @@ import '../components/languagenavigation/languagenavigation.js';
 import '../components/layout-container/modal.js';
 import '../components/navigation/navigation.js';
 import '../components/form/sign-in-buttons/sign-in-buttons.js';
-/*
-import '../components/form/sign-in-form/sign-in-form.js';*/
+import '../components/form/sign-in-form/sign-in-form.js';
 import '../components/contentfragment/js/contributor.js';
