39af65352d4cda153d150414340b5d4ddde0864f
Daniel Gordon <dgordon@adobe.com>
2020-11-14 12:11:27 -0800

    added redirect to current page for succesful sign in

--- ui.frontend/src/main/webpack/components/layout-container/modal.js
+++ ui.frontend/src/main/webpack/components/layout-container/modal.js
@@ -14,7 +14,7 @@ jQuery(function($) {
     /**
      * Handle clicking "off-modal" to hide it
      */
-    $(document).click(hideModal);
+    $(document).on('click', (hideModal));
 
 
     function showModal(e) {
@@ -23,12 +23,16 @@ jQuery(function($) {
         const xfUrl = $(this).data('modal-url');
 
         if (visible || !xfUrl) { return; }
+        const showModalEvt = new Event('wknd-modal-show');
+        const body = document.querySelector('body');
 
         $.get(xfUrl, function (data) {
             const modal = $('<div id="wknd-modal"/>');
             $('body').append(modal.append(data));
             modal.fadeIn(300, function() { visible = true; });
             visible = true;
+            //dispatch event
+            body.dispatchEvent(showModalEvt);
         });
 
         return false;
@@ -36,7 +40,8 @@ jQuery(function($) {
 
     function hideModal(e) {
         const modal = $('#wknd-modal');
-
+        const body = document.querySelector('body');
+        const hideModalEvt = new Event('wknd-modal-hide');
         // if the target of the click isn't the modal nor a descendant of the modal
         if (visible && modal && !modal.is(e.target) && modal.has(e.target).length === 0) {
             e.preventDefault();
@@ -44,6 +49,7 @@ jQuery(function($) {
             modal.fadeOut(200, function(){
                 modal.remove();
                 visible = false;
+                body.dispatchEvent(hideModalEvt);
             });
 
             return false;
