b1d3729070c54e2f7605d1d32b5a30aa68d00b2f
David Gonzalez <davidjgonzalez@gmail.com>
2020-11-24 16:56:03 -0500

    #175 - Switch from DomNodeInserted event handler to MutationObserver

--- ui.frontend/src/main/webpack/components/contentfragment/js/contributor.js
+++ ui.frontend/src/main/webpack/components/contentfragment/js/contributor.js
@@ -18,25 +18,49 @@
 
 (function() { 
 
-    function applyComponentStyles() {
-        document.querySelectorAll(".cmp-contentfragment--contributor .cmp-contentfragment[data-cmp-contentfragment-model=\"wknd/models/contributors\"]:not([data-cmp-contributor-processed='true'])").forEach(function (cf) {
+    /**
+     * This method applies the JavaScript-based "styling", or really, DOM adjustments required by the contributor style.
+     * 
+     * @param {*} responsiveGridEl the responsive grid element to look under for instances of content fragments that have the contributor style applied
+     */
+    function applyComponentStyles(responsiveGridEl) {
+        responsiveGridEl.querySelectorAll(".cmp-contentfragment--contributor .cmp-contentfragment[data-cmp-contentfragment-model=\"wknd/models/contributor\"]:not([data-cmp-contributor-processed='true'])").forEach(function (cf) {
+            // Mark the content fragment as processed, since we don't want to accidentally apply the JS adjustments multiple times
             cf.setAttribute("data-cmp-contributor-processed", true);
 
+            // Adjust the DOM, in this case injecting an img node and settings its source to the the content fragment's picture URL
             var cfEls = cf.querySelector('.cmp-contentfragment__elements');
             var assetPath = cfEls.querySelector(".cmp-contentfragment__element--pictureReference .cmp-contentfragment__element-value").innerText.trim();
 
             if (assetPath && assetPath.indexOf("/content/dam/") === 0) {
-                var pictureElement = document.createElement("img"); 
+                var pictureEl = document.createElement("img"); 
 
-                pictureElement.setAttribute("class","cmp-contentfragment__picture"); 
-				pictureElement.setAttribute("src", assetPath);
+                pictureEl.setAttribute("class", "cmp-contentfragment__picture"); 
+				pictureEl.setAttribute("src", assetPath);
 
-                cfEls.insertBefore(pictureElement, cfEls.querySelector(".cmp-contentfragment__element--fullName"));
+                cfEls.insertBefore(pictureEl, cfEls.querySelector(".cmp-contentfragment__element--fullName"));
             }
         });
     }
 
-    applyComponentStyles();
+    // Since mutation observers can only watch a single node (and not a node list), we'll register a mutation observer for each responsive grid
+    // on the page, as we are no sure which responsive grid a contributor-style content fragment might be added to.
+    document.querySelectorAll(".responsivegrid").forEach(function(responsiveGridEl) {
+        // Initialize the component styles on page load
+        applyComponentStyles(responsiveGridEl);
 
-    document.querySelectorAll(".responsivegrid").forEach(function(el) { el. addEventListener("DOMNodeInserted", applyComponentStyles); });
+         // Attach a mutation observer to handle drag and drop in page editor and the styling/authoring of components
+         // This is only required in the context of authoring, and could be split out to only execute in the context of the Page Editor.
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.type === "childList") {
+                    applyComponentStyles(responsiveGridEl);
+                }
+            });
+        });
+
+        // Observe changes to nodes under each responsive grid on the page
+        observer.observe(responsiveGridEl, 
+            { attributes: false, childList: true, characterData: false, subtree: true });
+    });
 })();
