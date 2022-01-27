d433575c5974d7d46e5f6c897dd7a3dfaeb6eef7
Khushbu Nayak <khushbunayak0725@gmail.com>
2022-01-27 18:20:32 +0530

    My custom conponent changes

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/UserDetailsModel.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2015 Adobe Systems Incorporated
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.adobe.aem.guides.wknd.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class)
+public class UserDetailsModel {
+
+   @Inject 
+	private String firstname;
+	
+	@Inject
+	private String lastname;
+	
+	@Inject
+	private String detail;
+	
+	
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+    /**
+	 * @return the lastName
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+     /**
+	 * @return the detail
+	 */
+	public String getdetail() {
+		return detail;
+	}
+
+}
