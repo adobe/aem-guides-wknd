31e5f338ec84026f3d16e2e1ae06688de6edef46
Sachin Mali <sachinpree@gmail.com>
2024-09-13 11:51:18 -0400

    Install and use 3rd party bundle service

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlet/SayHello.java
@@ -0,0 +1,32 @@
+package com.adobe.aem.guides.wknd.core.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.ServletResolverConstants;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import com.example.services.HelloWorldService;
+
+@Component(service = Servlet.class, property = {
+    ServletResolverConstants.SLING_SERVLET_PATHS + "=/bin/sayhello",
+    ServletResolverConstants.SLING_SERVLET_METHODS + "=" + HttpConstants.METHOD_GET
+})
+public class SayHello extends SlingSafeMethodsServlet {
+    
+        private static final long serialVersionUID = 1L;
+
+        @Reference
+        private HelloWorldService helloWorldService;
+    
+        @Override
+        protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+            response.getWriter().write("My-Example-Bundle service says: " + helloWorldService.sayHello());
+        }
+}
